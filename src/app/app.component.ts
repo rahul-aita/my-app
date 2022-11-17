d6a219368ec0b4a9f90d29a93c1b248ff41a55ce
RahulKumar <rahul@aitatechnologies.org>
2022-11-17 14:23:06 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { MatPaginator } from "@angular/material/paginator";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'my-app';
+  isTrue:boolean=true;
+  isTrueSignIn:boolean=true
+  ngOnInit(): void {
+    
+  }
+
+  close() {
+    this.isTrue=false;
+    this.isTrueSignIn=false
+    }
+    closedAll(){
+this.isTrueSignIn=true
+this.isTrue=false
+    }
 }
